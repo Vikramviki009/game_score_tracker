0e3fc05db78814377078f2f03b0040be3dd97d2b
Vikram Dabali <Vikramviki09@gmail.com>
2021-07-09 13:52:48 +0530

    added updated form file to handle changes on input and added link from react router dom to carry props to the scoreboard page

--- src/form.jsx
+++ src/form.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import './form.css';
+import Player from './player';
 import { Link } from 'react-router-dom';
 
 const FormPlayer = () => {
     const [ player, setPlayer ] = useState("")
     const [ player2, setPlayer2 ] = useState("")
 
+    const handleChange1 = (e) => {
+        setPlayer(e.target.value)
+    }
+
+    const handleChange2 = (e) => {
+        setPlayer2(e.target.value)
+    }
 
     return(
         <div className="form">
             <div className="forms">
                 <h1>Ping Pong Tracker</h1>
                 <form>
-                    <h1>Player</h1>
-                    <h1>Player2</h1>
+                    <Player player='Player1' handleChange={handleChange1} />
+                    <Player player='Player2' handleChange = {handleChange2} />
                     <br />
                     <Link to={{
                         pathname: '/scoreboard',
--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import PlayerContainer from './player-container'
+import { Link } from 'react-router-dom';
 
-function Scoreboard() {
-    return (
-        <div>
-            scoreboard   
-        </div>
+const Scoreboard = (props) => {
+    const { player, player2 } = props.location.state;
+    const [score, setScore] = useState(0);
+    const [score2, setScore2] = useState(0);
+
+    const add1 = () => {
+        let n = Number(score);
+        n = n+1;
+
+        return setScore(n);
+    }
+
+    const add2 = () => {
+        let n = Number(score2);
+        n = n+1;
+        return setScore2(n)
+    };
+
+
+    const gameWinner = () => {
+        if (score > score2){
+            return player
+        }else if(score < score2){
+            return player2
+        }else if(score && score2 > 0 && score === score2){
+            return 'Draw match!!! Play One more time'
+        }
+        return 'Play the Game to decide'
+    }
+
+    const winDifference = (n1, n2) => {
+        if(n1 > n2 ){
+            return n1-n2;
+        }else{
+           return n2-n1
+        }
+    }
+
+    return(
+        <>
+            <h1>Scoreboard</h1>
+            {
+                player && player2 ?
+                <div>
+                    <PlayerContainer player={player} add={add1} score={score} />
+                    <PlayerContainer player= {player2} add={add2} score={score2} />
+                    <div className="conclusion">
+                        <p>Game Winner: {gameWinner()}</p>
+                        <p>Win Difference: {winDifference(score, score2)}</p>
+                    </div>
+                </div> :
+                (<h2>Please enter both player names</h2>)
+            }
+            <Link className="link" to='/'>Reset</Link>
+        </>
     )
 }
-
 export default Scoreboard
